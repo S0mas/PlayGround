d3fe52a4b834b7c56281ba4d5e2d015ce4df8b10
S0mas <sommerfeld.krzysztof@gmail.com>
2018-12-19 16:23:27 +0100

    add PythagoreanTriplesGenerator

---
+++ NewCPPFeatures/NewCPPFeatures/LazyPythagoreanTriplesGenerator.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <cmath>
+#include <functional>
+
+/*
+static constexpr int mySqrt(const int res) {
+	const std::function<int(int, int, int)>& mySqrtHelper = [&mySqrtHelper](const int res, const int l, const int r) constexpr {
+		if (l == r)
+			return r;
+		const auto mid = (r + l) / 2;
+		if (mid * mid >= res)
+			return mySqrtHelper(res, l, mid);
+		return mySqrtHelper(res, mid + 1, r);
+	};
+	return mySqrtHelper(res, 1, res);
+}
+*/
+
+static bool isSqrtRootInteger(const int number, double& root)
+{
+	if (number == 0) return false;
+	root = std::sqrt(number);
+	return root == static_cast<int>(root);
+}
+
+class PythagoreanTriplesGenerator {
+	static auto getPythagoreanTriplesGenerator_(const int x, const int z) {
+		return [x=x, z=z]() mutable {
+			goto resume;
+			while (true) {
+				for (; ; ++z) {
+					for (x = 1; x < z; ++x) {
+						resume:
+						double root = 0;
+						if (isSqrtRootInteger(pow(z, 2) - pow(x, 2), root))
+							return PythagoreanTriple(x++, root, z);
+					}
+				}
+			}
+		};
+	}
+public:
+	static auto getPythagoreanTriplesGenerator(const int startId) {
+		auto i = 0;
+		while(true) {
+			for(auto z = 2; ; ++z) {
+				for(auto x = 1; x < z; ++x) {
+					double root = 0;
+					if (isSqrtRootInteger(pow(z, 2) - pow(x, 2), root))
+						++i;
+					if(i >= startId)
+						return getPythagoreanTriplesGenerator_(x, z);
+				}
+			}
+		}
+	}
+
+	struct PythagoreanTriple {
+		PythagoreanTriple(const int x, const int y, const int z) : x_(x), y_(y), z_(z) {}
+		const int x_;
+		const int y_;
+		const int z_;
+	};
+};
+
+inline std::ostream& operator<<(std::ostream &output, const PythagoreanTriplesGenerator::PythagoreanTriple &p) {
+	output << "X: " << p.x_ << " Y: " << p.y_ << " Z: " << p.z_ << '\n';
+	return output;
+}
+
--- NewCPPFeatures/NewCPPFeatures/NewCPPFeatures.cpp
+++ NewCPPFeatures/NewCPPFeatures/NewCPPFeatures.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include "LazyFiboNumbersGenerator.h"
-
+#include "LazyPythagoreanTriplesGenerator.h"
 /*generator<int> generatorForNumbers(int begin, int inc = 1) {
 
 	for (int i = begin;; i += inc) {
@@ -11,10 +11,26 @@
 
 }*/
 
+void f() {
+  while (true) {
+    for (auto z = 2;; ++z) {
+      auto i = 0;
+      for (auto x = 1; x < z; ++x) {
+        double root = 0;
+        if (isSqrtRootInteger(pow(z, 2) - pow(x, 2), root)) {
+          ++i;
+          std::cout << "X: " << x << " Y: " << root << " Z: " << z << '\n';
+          if (i > 1) getchar();
+        }
+      }
+    }
+  }
+}
+
 int main() {
 	auto fibNumbersPass10 = FiboSeqGenerator::getFiboGenerator(11);
 	auto fibNumbers = FiboSeqGenerator::getFiboGenerator(1);
-
+	auto pythTriplesGenerator = PythagoreanTriplesGenerator::getPythagoreanTriplesGenerator(1);
 	for (auto i = 0; i < 10; ++i) 
 		std::cout << fibNumbersPass10() << " ";
 
@@ -22,6 +38,11 @@ int main() {
 
 	for (auto i = 0; i < 20; ++i) 
 		std::cout << fibNumbers() << " ";
+
+	std::cout << "\n\n";
+
+	for (auto i = 0; i < 10; ++i)
+		std::cout << pythTriplesGenerator() << "\n";
 	getchar();
 	return 0;
 
